efc5026e42f300ca32ebf4dfca1bad1b36f2b0e3
Barba <joao.panazzolo23@gmail.com>
2024-10-12 21:27:23 -0300

    Apagar Tarefas

--- AplicativoMaiu/Views/Tarefas.xaml.cs
+++ AplicativoMaiu/Views/Tarefas.xaml.cs
@@ -1,40 +1,50 @@
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
-namespace AplicativoMaiu;
-public class Tarefa
+namespace AplicativoMaiu
 {
-public bool Concluida { get; set; }
-
-public string Nome { get; set; }
-
-}
-public partial class Tarefas : ContentPage
-{
-    // ObservableCollection para atualizar a lista automaticamente
-    ObservableCollection<Tarefa> tasks;
-
-    public Tarefas()
+    public class Tarefa
     {
-        InitializeComponent();
-        tasks = new ObservableCollection<Tarefa>();
-        taskListView.ItemsSource = tasks;
+        public bool Concluida { get; set; }
+        public string Nome { get; set; }
     }
 
-    // adicionar nova tarefa
-    private void OnAddTaskClicked(object sender, EventArgs e)
+    public partial class Tarefas : ContentPage
     {
-        string newTask = taskEntry.Text;
+        public ObservableCollection<Tarefa> Tasks { get; set; }
 
-        if (!string.IsNullOrWhiteSpace(newTask))
+        public ICommand RemoverTarefaCommand => new Command<Tarefa>((tarefa) => RemoverTarefa(tarefa));
+        public Tarefas()
         {
-            var tarefa = new Tarefa { Nome = newTask, Concluida = false };
-            tasks.Add(tarefa);
-            Console.WriteLine($"Tarefa adicionada: {tarefa.Nome}"); 
-            taskEntry.Text = string.Empty;
+            InitializeComponent();
+            Tasks = new ObservableCollection<Tarefa>();
+            taskListView.ItemsSource = Tasks;
+            BindingContext = this;
         }
-        else
+
+        private void RemoverTarefa(Tarefa tarefa)
         {
-            DisplayAlert("Erro", "A tarefa não pode estar vazia", "OK");
+            if (tarefa != null && Tasks.Contains(tarefa))
+            {
+                Tasks.Remove(tarefa);
+                Console.WriteLine("Tarefa removida!");
+            }
+        }
+
+        private void AdicionarNovaTarefa(object sender, EventArgs e)
+        {
+            string newTask = taskEntry.Text;
+            if (!string.IsNullOrWhiteSpace(newTask))
+            {
+                var tarefa = new Tarefa { Nome = newTask, Concluida = false };
+                Tasks.Add(tarefa);
+                Console.WriteLine($"Tarefa adicionada: {tarefa.Nome}");
+                taskEntry.Text = string.Empty;
+            }
+            else
+            {
+                DisplayAlert("Erro", "A tarefa não pode estar vazia", "OK");
+            }
         }
     }
 }
